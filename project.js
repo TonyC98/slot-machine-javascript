569d3c248ee55b03f20adec83d5d7c104850fb99
Antonio Caricati <140312553+TonyC98@users.noreply.github.com>
2023-10-27 16:48:13 +0700

    added list of tasks using comments

---
+++ project.js
@@ -0,0 +1,7 @@
+// 1. Deposit some money
+// 2. Determinate number of lines to bet in
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winning
+// 7. Play again
