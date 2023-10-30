8e9655ed5badd6937f43f53c50ad9526926b1816
Antonio Caricati <140312553+TonyC98@users.noreply.github.com>
2023-10-30 20:46:22 +0000

    added getBet function

--- project.js
+++ project.js
@@ -24,7 +24,6 @@ const getNumberOfLines = () => {
   while (true) {
     const lines = prompt("Enter the number of lines to bet on (1-3): ")
     const numberOfLines = parseFloat(lines)
-    
     if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
       console.log("Invalid number of lines, try again.")
     } else {
@@ -33,5 +32,18 @@ const getNumberOfLines = () => {
   }   
 }
 
-const depositAmount = deposit()
+const getBet = (balance) => {
+  while (true) {
+    const bet = prompt("Enter the total bet: ")
+    const numberBet = parseFloat(bet)
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance) {
+      console.log("Invalid bet, try again.")
+    } else {
+      return numberBet
+    }
+  }   
+}
+
+let balance = deposit()
 const numberOfLines = getNumberOfLines()
+const bet = getBet(balance)
