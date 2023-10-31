5981eade3b007a123d595c666fdd08d4800f7af4
Antonio Caricati <140312553+TonyC98@users.noreply.github.com>
2023-10-31 18:48:43 +0000

    added for loops to get symbol and push it into new array

--- project.js
+++ project.js
@@ -66,8 +66,18 @@ const spin = () => {
   for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
     for (let i = 0; i < count; i++) {
       symbols.push(symbol)
-      console.log(symbols)
     }
+  }
+  const reels = [[], [], []]
+  for (let i = 0; i < COLS; i++) {
+    const reelSymbols = [...symbols]
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length)
+      const selectedSymbol = reelSymbols[randomIndex]
+      reels[i].push(selectedSymbol)
+      
+    }
+    
   }
 }
 
