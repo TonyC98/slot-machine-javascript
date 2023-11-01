fbef36c12194b8e7b7e84844918c2e0324fd374d
Antonio Caricati <140312553+TonyC98@users.noreply.github.com>
2023-11-01 08:45:29 +0000

    added get winnings function and winning statement

--- project.js
+++ project.js
@@ -106,6 +106,26 @@ const printRows = (rows) => {
   }
 }
 
+const getWinnings = (rows, bet, lines) => {
+  let winnings = 0
+
+  for (let row = 0; row < lines; row++) {
+    const symbols = rows[row]
+    let allSame = true
+
+    for (const symbol of symbols) {
+      if (symbol != symbol[0]) {
+        allSame = false
+        break
+      }
+    }
+    
+    if (allSame) {
+      winnings += bet * SYMBOL_VALUES[symbols[0]]
+    }
+  }
+}
+
 let balance = deposit()
 const numberOfLines = getNumberOfLines()
 const bet = getBet(balance, numberOfLines)
